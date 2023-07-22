a0075ece41785b54c43ad25c673d83979c92a4c1
Akshat Chaudhari <cakshat01@gmail.com>
2023-07-22 10:45:29 +0530

    Update train.py

--- src/model/train.py
+++ src/model/train.py
@@ -11,6 +11,7 @@ import pandas as pd
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 
+mlflow.autolog()
 
 # define functions
 def main(args):
